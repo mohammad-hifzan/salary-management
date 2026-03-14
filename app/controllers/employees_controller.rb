8f55491920ec583d0731235bfe3be9e6e83c7289
Mohammad Hifzan <misterj435@gmail.com>
2026-03-14 15:43:15 +0530

    implemented employee crud endpoint

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,7 +1,29 @@
 class EmployeesController < ApplicationController
+  def index
+    employees = Employee.all
+    render json: employees
+  end
+
+  def show
+    employee = Employee.find(params[:id])
+    render json: employee
+  end
+
   def create
     employee = Employee.create!(employee_params)
     render json: employee, status: :created
+  end
+
+  def update
+    employee = Employee.find(params[:id])
+    employee.update!(employee_params)
+    render json: employee
+  end
+
+  def destroy
+    employee = Employee.find(params[:id])
+    employee.destroy
+    head :no_content
   end
 
   private
